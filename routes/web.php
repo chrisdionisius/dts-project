a43553fee62cb411145fee1b629b45b54e116299
Chris Dionisius <chrisdionisius@gmail.com>
2020-10-14 19:07:57 +0700

    add template home

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home','HomeController');
+Route::get('/post/{laman}',['uses'=>'PostController@post']);
 // Route::get('/home', 'HomeController@index')->name('home');
